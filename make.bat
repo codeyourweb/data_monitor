eba5fdd314f0c9643b028f57e1134743f35d7b89
codeyourweb <dev.jp.garnier@gmail.com>
2025-09-11 15:13:40 +0200

    Remove -s -w attribs (usefull for process injection)

--- make.bat
+++ make.bat
@@ -2,5 +2,5 @@ set GOARCH=amd64
 set CGO_ENABLED=1
 del DataMonitor.dll
 del DataMonitor.h
-go build -ldflags="-H=windowsgui -s -w" -tags=dll_export --buildmode=c-shared -o DataMonitor.dll .
+go build -ldflags="-H=windowsgui" -tags=dll_export --buildmode=c-shared -o DataMonitor.dll .
 pause
